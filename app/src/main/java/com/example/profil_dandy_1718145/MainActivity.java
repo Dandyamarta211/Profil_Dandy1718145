6da77a3c623f22641bfe7b1a95bfd266c5a35852
andrew sanjaya <andrewsanjaya11@gmail.com>
2019-10-15 00:18:31 +0700

    Initial commit

---
+++ app/src/main/java/com/example/profil_dandy_1718145/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.profil_dandy_1718145;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
